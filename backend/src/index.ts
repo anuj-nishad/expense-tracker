d30115750c87dc771076b540d3837b78594d3c35
anuj-nishad <nishadanuj995@gmail.com>
2025-08-06 21:47:03 +0530

    Initial commit: Project setup

---
+++ backend/src/index.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import type {Request, Response} from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json())
+
+app.get("/",(req: Request, res: Response)=>{
+  res.send("Hello from Typescript backend");
+});
+
+app.listen(PORT, ()=>{
+  console.log(`Server is  running on ${PORT}`)
+})
