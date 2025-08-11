493e939b0f69619032da87b570db4edddc56665e
anuj-nishad <nishadanuj995@gmail.com>
2025-08-11 21:35:39 +0530

    setting up styles

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -5,7 +5,7 @@ export default function Index() {
   return (
     <View>
       <Image source={require("@/assets/images/react-logo.png")} style={{ width: 100, height: 100 }} />
-      <Text>Hello everyone how are you all</Text>
+      <Text>Hello everyone how are you all </Text>
     </View>
   );
 }
