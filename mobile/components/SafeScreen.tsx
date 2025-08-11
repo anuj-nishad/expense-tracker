6084e8ca4f796af15852120ca56653844162974c
anuj-nishad <nishadanuj995@gmail.com>
2025-08-11 22:07:05 +0530

    use @/alias instead of relative support

--- mobile/components/SafeScreen.tsx
+++ mobile/components/SafeScreen.tsx
@@ -1,7 +1,7 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import { COLORS } from '@/constants/colors';
+import { COLORS } from "@/constants/colors";
 
 type SafeScreenProps = React.PropsWithChildren<{}>;
 
