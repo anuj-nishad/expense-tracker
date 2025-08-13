27f8e075b873bb367bd403bc99b81c8a61798fd0
anuj-nishad <nishadanuj995@gmail.com>
2025-08-14 01:19:38 +0530

    Fixed

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,11 +2,13 @@ import express from "express";
 import dotenv from "dotenv";
 import transactionsRoutes from "./routes/transactionsRoutes.js";
 import rateLimiter from "./middleware/rateLimiter.js";
+import job from "./config/cron.js";
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+if(process.env.NODE_ENV === "production") job.start();
 app.use(rateLimiter);
 app.use(express.json())
 
